041ad421c3409c13c276a95e1080231c0b63a135
David Kretch <david.kretch@gmail.com>
2016-10-29 17:35:22 -0400

    Add color mapping, group by

--- test/test.js
+++ test/test.js
@@ -1,9 +1,17 @@
-var data = [];
-for (let i = 0; i < 100; i++) {
-  data.push({x: Math.random(), y: Math.random()});
+// Returns a random integer between min (included) and max (included)
+// Using Math.round() will give you a non-uniform distribution!
+function randomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-var p = new Plot(data, {x: "x", y: "y"}, selector = "div.plot", width = 600, height = 400).
+var data = [];
+for (let i = 0; i < 100; i++) {
+  data.push({x: Math.random(), y: Math.random(), group: randomInt(1, 3).toString()});
+}
+
+var p = new Plot(data, {x: "x", y: "y", color: "group"}, selector = "div.plot", width = 600, height = 400).
   geomPoint();
   
 p.show();
